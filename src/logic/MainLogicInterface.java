39ad63ac2740e94216f7008740f63985ae991983
achmaddav <achmaddavit92@gmail.com>
2022-12-14 22:14:11 +0700

    Logic Interface Logic 01 Soal 02

--- src/logic/MainLogicInterface.java
+++ src/logic/MainLogicInterface.java
@@ -2,6 +2,7 @@ package logic;
 
 import logic.logicInterface.LogicInterface;
 import logic.logicInterface.logic01Impl.Logic01Soal01Impl;
+import logic.logicInterface.logic01Impl.Logic01Soal02Impl;
 
 public class MainLogicInterface {
     public static void main(String[] args) {
@@ -9,5 +10,9 @@ public class MainLogicInterface {
         System.out.println("Sample Logic 01 Soal 01");
         LogicInterface logic01Soal01 = new Logic01Soal01Impl(new BasicLogic(9));
         logic01Soal01.cetakArray();
+
+        System.out.println("\n\nSample Logic 01 Soal 02");
+        LogicInterface logic01Soal02 = new Logic01Soal02Impl(new BasicLogic(9));
+        logic01Soal02.cetakArray();
     }
 }
